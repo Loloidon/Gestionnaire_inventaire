67acec8c5c96e9c12f5bbc53f19bd5199a7140ad
Loloidon <thijssenloic@gmail.com>
2026-07-06 14:55:42 +0200

    Changement de type de propriétée pour Phone de Supplier

---
+++ Gestionnaire_inventaire/Model/Supplier.cs
@@ -0,0 +1,10 @@
+﻿namespace Gestionnaire_inventaire.Model
+{
+    public class Supplier
+    {
+        public int Id { get; set; }
+        public string CompanyName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+    }
+}
