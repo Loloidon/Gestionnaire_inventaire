a3a5276578924f3bec026a02faef4cf9af6a4e1b
Loloidon <thijssenloic@gmail.com>
2026-07-07 13:20:50 +0200

    Ajout de supplier et du Get dans l'API

---
+++ Gestionnaire_inventaire/Controllers/SupplierController.cs
@@ -0,0 +1,25 @@
+﻿using Gestionnaire_inventaire.Data;
+using Gestionnaire_inventaire.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+namespace Gestionnaire_inventaire.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class SupplierController:ControllerBase
+    {
+
+        private readonly InventoryContext _context;
+
+        public SupplierController(InventoryContext context)
+        {
+            _context = context;
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Supplier>>> GetSuppliers()
+        {
+            return await _context.Suppliers.ToListAsync();
+        }
+    }
+}
