13efaeb35ff1c938c7fc66f1e16136ad34f96758
Loloidon <thijssenloic@gmail.com>
2026-07-07 13:09:19 +0200

    Ajout de CategoryController et du GET dans l'api

---
+++ Gestionnaire_inventaire/Controllers/CategoryController.cs
@@ -0,0 +1,26 @@
+﻿using Gestionnaire_inventaire.Data;
+using Gestionnaire_inventaire.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Net.Http.Headers;
+
+namespace Gestionnaire_inventaire.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CategoryController: ControllerBase
+    {
+    private readonly InventoryContext _context;
+    public CategoryController(InventoryContext context)
+        {
+            _context = context;
+        }
+    
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<Category>>> GetCategory()
+    {
+        return await _context.Categories.ToListAsync();
+    }
+    }
+    
+}
