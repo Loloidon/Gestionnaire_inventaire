58a7b6a3f3f67c3cc77c603b98a19636ee982235
Loloidon <thijssenloic@gmail.com>
2026-07-07 11:46:35 +0200

    Ajout du ProductController et Création du get dans mon api.

---
+++ Gestionnaire_inventaire/Controllers/ProductsController.cs
@@ -0,0 +1,25 @@
+﻿using Gestionnaire_inventaire.Data;
+using Gestionnaire_inventaire.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gestionnaire_inventaire.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductsController : ControllerBase
+    {
+        private readonly InventoryContext _context;
+
+        public ProductsController(InventoryContext context)
+        {
+            _context = context;
+
+        }
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
+        {
+            return await _context.Products.ToListAsync();
+        }
+    }
+}
