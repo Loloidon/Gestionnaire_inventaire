6a7837c45ac018dea1a4dddfcf68220a10fe2b21
Loloidon <thijssenloic@gmail.com>
2026-07-08 15:43:08 +0200

    Ajout de SupplierDto

---
+++ Gestionnaire_inventaire/DTO/SupplierDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Gestionnaire_inventaire.DTO
+{
+    public class SupplierDto
+    {
+        public string CompanyName { get; set; } = "";
+        public string Email { get; set; } = "";
+        public string Phone { get; set; } = "";
+    }
+}
