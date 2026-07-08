da89d0cdb53afb17bbd7a25a57dcd4ca722366d5
Loloidon <thijssenloic@gmail.com>
2026-07-08 14:35:00 +0200

    Ajout de la validation dans Category.

--- Gestionnaire_inventaire/Model/Category.cs
+++ Gestionnaire_inventaire/Model/Category.cs
@@ -1,8 +1,13 @@
-﻿namespace Gestionnaire_inventaire.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestionnaire_inventaire.Model
 {
     public class Category
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = "";
 
         public ICollection<Product> Products = new List<Product>();
