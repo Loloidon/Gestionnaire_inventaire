a887f8b3bca7a5d6ff08fdef6319c202d038f097
Loloidon <thijssenloic@gmail.com>
2026-07-08 14:37:49 +0200

    Ajout de validation a Product

--- Gestionnaire_inventaire/Model/Product.cs
+++ Gestionnaire_inventaire/Model/Product.cs
@@ -1,11 +1,21 @@
-﻿namespace Gestionnaire_inventaire.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Gestionnaire_inventaire.Model
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(100)]
         public string Name { get; set; } = "";
+        [Required]
+        [StringLength(500)]
         public string Description { get; set; } = "";
+        [Required]
+        [Range(0.01, 100000)]
         public decimal Price { get; set; }
+        [Required]
+        [Range(0,int.MaxValue)]
         public int StockQuantity { get; set; }
         public DateTime CreatedAt { get; set; } = DateTime.Now;
         public int CategoryId { get; set; }
