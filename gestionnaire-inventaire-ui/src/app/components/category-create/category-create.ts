9317727f3778109a66aa1fdd8233f9aa6e610e55
Loloidon <thijssenloic@gmail.com>
2026-09-14 14:38:46 +0200

    WIP category create

--- gestionnaire-inventaire-ui/src/app/components/category-create/category-create.ts
+++ gestionnaire-inventaire-ui/src/app/components/category-create/category-create.ts
@@ -1,9 +1,22 @@
 import { Component } from '@angular/core';
-
+import { signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { Category } from '../../models/category';
+import { FormControl, FormGroup } from '@angular/forms';
+import { CategoryService } from '../../services/category-service';
 @Component({
   selector: 'app-category-create',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category-create.html',
   styleUrl: './category-create.css',
 })
-export class CategoryCreate {}
+export class CategoryCreate {
+  categories=signal<Category[]>([]);
+  categoryForm= new FormGroup({
+    name: new FormControl('')
+  })
+constructor(categoryService : CategoryService)
+{
+  
+}
+}
