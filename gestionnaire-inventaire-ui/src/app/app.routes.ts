e5debb2983ff98c676014f12448fa9ef2faa6154
Loloidon <thijssenloic@gmail.com>
2026-09-14 13:43:13 +0200

    Complétion de category-list et ajout du composant Category-create.

--- gestionnaire-inventaire-ui/src/app/app.routes.ts
+++ gestionnaire-inventaire-ui/src/app/app.routes.ts
@@ -2,6 +2,7 @@ import { Routes } from '@angular/router';
 import { ProductList } from './components/product-list/product-list';
 import { ProductCreate } from './components/product-create/product-create';
 import { ProductEdit } from './components/product-edit/product-edit';
+import { CategoryList } from './components/category-list/category-list';
 export const routes: Routes = [
     {
         path:'products',
@@ -14,5 +15,9 @@ export const routes: Routes = [
     {
         path: 'products/edit/:id',
         component: ProductEdit
+    },
+    {
+        path: 'categories',
+        component:CategoryList
     }
 ];
--- gestionnaire-inventaire-ui/src/app/components/category-list/category-list.ts
+++ gestionnaire-inventaire-ui/src/app/components/category-list/category-list.ts
@@ -1,9 +1,36 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
+import { signal } from '@angular/core';
+import { Category } from '../../models/category';
+import { CategoryService } from '../../services/category-service';
 
 @Component({
   selector: 'app-category-list',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './category-list.html',
   styleUrl: './category-list.css',
 })
-export class CategoryList {}
+export class CategoryList {
+  category= signal<Category[]>([]);
+
+  constructor(private categoryService:CategoryService)
+  {
+
+  }
+  ngOnInit(){
+    this.categoryService.getCategories().subscribe(category=>{
+      console.log("Réponse API :", category);
+      this.category.set(category);
+      console.log("LOCAL :", this.category);
+
+    });
+  }
+  deleteCategory(id:number){
+    this.categoryService.deleteCategory(id).subscribe(()=>{
+      this.category.update(category=>
+        category.filter(category=>category.id !== id)
+        
+      );
+    });
+  }
+}
